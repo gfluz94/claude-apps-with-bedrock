af3df623fbe957dd627a3887c9752faec2934ddd
gabriel <gfluz94@gmail.com>
2025-02-16 17:36:44 +0100

    add chat interface with streamlit

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
+import streamlit as st
+
 from utils.bedrock import BedrockAgent, AgentType
+from utils.query import build_query
+
 
 if __name__ == "__main__":
+    with st.sidebar:
+        st.title(" Chat Configuration")
+        st.header("Clear Chat History")
+        if st.button("Clear Chat"):
+            st.session_state.messages = []
+
+
+    st.title("Chat with Bedrock")
+    question = st.chat_input(placeholder="Type a message...")
+
+    agent_type = AgentType.QA
+    query = build_query(agent_type, question)
+    
     agent = BedrockAgent(agent_type=AgentType.QA)
-    response = agent.answer("What is the meaning of life?")
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["text"])
+
+    if question:
+        st.session_state.messages.append({"role": "User", "text": question})
+        with st.chat_message("User"):
+            st.write(question)
+        response = agent.answer(query)
+        st.session_state.messages.append({"role": "Assistant", "text": response})
+        with st.chat_message("Assistant"):
+            st.write(response)
---
+++ utils/query.py
@@ -0,0 +1,13 @@
+from string import Template
+
+from utils.bedrock import AgentType
+from utils.settings import CONFIG
+
+
+def build_query(agent_type: AgentType, question: str) -> str:
+    if agent_type == AgentType.QA:
+        prompt_template = Template(CONFIG["Prompts"]["User"]["QA"])
+        query = prompt_template.substitute(question=question)
+    else:
+        raise ValueError(f"Agent type {agent_type} not supported.")
+    return query
